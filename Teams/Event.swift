48d286cae886c2e14c9043d1456dd4f4e40e8eb7
Amy <amyshen@berkeley.edu>
2017-04-05 17:09:00 -0700

    i hope this undos the damage

--- Teams/Event.swift
+++ Teams/Event.swift
@@ -20,7 +20,6 @@ class Event {
     var peopleGoing: [String] = []
     var date: String?
     var location: String?
-
     
     //used to create fake events
     init(author: String, sport: String, description: String, peopleGoing: [String], date: String, location: String) {
@@ -30,5 +29,29 @@ class Event {
         self.peopleGoing = peopleGoing
         self.date = date
         self.location = location
+    }
+    
+    init(id: String, postDict: [String:Any]?) {
+        self.id = id
+        if postDict != nil {
+            if let description = postDict!["description"] as? String {
+                self.description = description
+            }
+            if let author = postDict!["author"] as? String {
+                self.author = author
+            }
+            if let authorPhoneNumber = postDict!["authorPhoneNumber"] as? Int {
+                self.authorPhoneNumber = authorPhoneNumber
+            }
+            if let sport = postDict!["sport"] as? String {
+                self.sport = sport
+            }
+            if let date = postDict!["date"] as? String {
+                self.date = date
+            }
+            if let peopleGoing = postDict!["peopleGoing"] as? [String] {
+                self.peopleGoing = peopleGoing
+            }
+        }
     }
 }
