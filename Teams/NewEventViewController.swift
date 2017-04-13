99e4ecc8520db163244be89bb95ce9f0421adf5b
shireenwarrier <shireen.warrier@gmail.com>
2017-04-12 22:26:32 -0700

    ui for new event

--- Teams/NewEventViewController.swift
+++ Teams/NewEventViewController.swift
@@ -28,6 +28,9 @@ class NewEventViewController: UIViewController {
         self.hideKeyboardWhenTappedAround()
         self.navigationController?.navigationBar.tintColor = UIColor.black
         setupLayout()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
     
@@ -36,15 +39,20 @@ class NewEventViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
     func setupLayout() {
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.init(red: 249/255, green: 170/255, blue: 97/255, alpha: 1.0)
         
         createTeamLabel = UILabel(frame: CGRect(x: 0, y: (navigationController?.navigationBar.frame.maxY)! + 10, width: view.frame.width, height: 75))
         createTeamLabel.textAlignment = .center
         createTeamLabel.text = "Create a Team"
-        createTeamLabel.font = UIFont(name: "ArialMT", size: 23)
+        createTeamLabel.font = UIFont(name: "ArialMT", size: 30)
         createTeamLabel.adjustsFontSizeToFitWidth = true
-        createTeamLabel.textColor = UIColor.black
+        createTeamLabel.textColor = UIColor.white
         
         sportPicker = UIPickerView(frame: CGRect(x: 0, y: createTeamLabel.frame.maxY + 10, width: view.frame.width, height: 120))
         sportPicker.delegate = self
@@ -54,22 +62,29 @@ class NewEventViewController: UIViewController {
         
         datePicker = UIDatePicker(frame: CGRect(x: 0, y: sportPicker.frame.maxY + 20, width: view.frame.width, height: 125))
         datePicker.addTarget(self, action: #selector(getDate), for: .valueChanged)
+        datePicker.setValue(UIColor.white, forKey: "textColor")
         
         locationTextField = UITextField(frame: CGRect(x: 0, y: datePicker.frame.maxY + 20, width: view.frame.width, height: 75))
         locationTextField.placeholder = "Enter Location"
         locationTextField.textAlignment = .center
+        locationTextField.textColor = UIColor.white
         
         descriptionField = UITextView(frame: CGRect(x: 0, y: locationTextField.frame.maxY + 20, width: view.frame.width, height: 100))
         descriptionField.text = "Description of Event"
         descriptionField.textAlignment = .center
         descriptionField.textContainer.maximumNumberOfLines = 2
-        descriptionField.textColor = UIColor.black
+        descriptionField.textColor = UIColor.white
+        descriptionField.font = UIFont(name: (descriptionField.font?.fontName)!, size: 20)
         descriptionField.isUserInteractionEnabled = true
         descriptionField.delegate = self
+        descriptionField.backgroundColor = UIColor.init(red: 249/255, green: 170/255, blue: 97/255, alpha: 1.0)
         
-        postButton = UIButton(frame: CGRect(x: 0, y: descriptionField.frame.maxY + 20, width: view.frame.width, height: 60))
+        postButton = UIButton(frame: CGRect(x: view.frame.width/5, y: descriptionField.frame.maxY + 10, width: view.frame.width * (3/5), height: 60))
         postButton.setTitle("Post", for: .normal)
-        postButton.setTitleColor(UIColor.black, for: .normal)
+        postButton.setTitleColor(UIColor.white, for: .normal)
+        postButton.titleLabel?.font = UIFont(name: (postButton.titleLabel?.font.fontName)!, size: 20)
+        postButton.layer.borderWidth = 3.0
+        postButton.layer.borderColor = UIColor.white.cgColor
         postButton.addTarget(self, action: #selector(addNewEvent), for: .touchUpInside)
         
         view.addSubview(createTeamLabel)
@@ -124,6 +139,11 @@ extension NewEventViewController: UIPickerViewDataSource, UIPickerViewDelegate, 
     
     func textViewDidBeginEditing(_ textView: UITextView) {
         descriptionField.text = ""
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let attributedString = NSAttributedString(string: sportsList[row], attributes: [NSForegroundColorAttributeName : UIColor.white])
+        return attributedString
     }
 }
 
