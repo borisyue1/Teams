67f47b1a507a173b724d660455492df77c4717b0
Boris Yue <borisyue@yahoo.com>
2017-04-15 17:19:25 -0700

    added settings VC

--- Teams/AppDelegate.swift
+++ Teams/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let _ = UserDefaults.standard.value(forKey: "name") {
             if let _1 = UserDefaults.standard.value(forKey: "school") {
                 //if name and school already inputted, skip to optionview
-                mainView = FeedViewController()
+                mainView = OptionViewController()
             }
         } else {
             mainView = SelectSchoolViewController()
